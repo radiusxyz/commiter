84d7a0d4a47003063c1696faf82bb2fa8ee0f72e
jayden050121 <jayden050121@gmail.com>
2024-01-29 09:07:15 +0000

    set up workspace

--- rest/src/main.rs
+++ rest/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use pointproof;
+
+#[get("/commitment")]
+fn commitment() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/test")]
+fn test() -> &'static str {
+    "Hello, test!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![commitment, test])
 }
